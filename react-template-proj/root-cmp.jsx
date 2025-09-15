2a30c4c73f3551606da1f6e2c7a646e360a8b075
yahavmor <yahavmor77@gmail.com>
2025-09-15 20:22:52 +0300

    init app

---
+++ react-template-proj/root-cmp.jsx
@@ -0,0 +1,12 @@
+export function App() {
+    return (
+        <section className="app">
+            <header className="app-header">
+                <h1>My App</h1>
+            </header>
+            <main className="container">
+                Hello React App
+            </main>
+        </section>
+    )
+}
