08d4859b67813b52ad45354c351e0edde20c9f86
yahavmor <yahavmor77@gmail.com>
2025-09-16 20:31:24 +0300

    ex4 - init app

--- react-template-proj/app.js
+++ react-template-proj/app.js
@@ -1,14 +1,19 @@
 import { CountDown } from './count-down.jsx'
 import { App } from './root-cmp.jsx'
 import { SeasonClock } from './season-clock-cmp.jsx'
+import { SelectedWatcher } from './selectedWatcher-cmp.jsx'
 import { Table } from './table-cmp.jsx'
+import { Watchers } from './watchers-cmp.jsx'
+
+
 
 const elContainer = document.getElementById('root')
 const root = ReactDOM.createRoot(elContainer)
 root.render(
       <div>
         <App />
-        <CountDown toTime={Date.now() + 1000*60} startFrom={10} onDone={()=>{ console.log('Done!') }} />
+        <Watchers />
+        <SelectedWatcher />
       </div>
 )
     
